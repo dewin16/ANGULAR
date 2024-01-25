70480f2f7cfdc7470d0ba834641411a02a3a48bf
Carlos <JoseCarlosCarnicero@gmail.com>
2024-01-25 16:38:05 +0100

    Se añaden funciones con flecha

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 //si se pone un nombre en el parametro de la funcion lo usa como predeterminado
-//en este caso paco seria el nombre determinado si no se define abajo juan
-function sayHello(name = "paco"){
+const sayHello = (name = "paco") =>`Hola mundo funcion ${name}`;
+//las dos expresiones hacen lo mismo, solo que al ser de una sola linea se puede simplificar
+const add = (a = 0, b = 0) => {
+    return a+b}
 
-    const saludo = `Hola mundo funcion ${name}`;
-    //console.log("Hola mundo funcion");
-
-    return saludo;
-}
-//const result = sayHello();
-//console.log(result);
-
-//tambien se puede imprimir directamente la ejecucion dela funcion
-console.log(sayHello("juan"));
+console.log(add(10,5));
