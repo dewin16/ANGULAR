03f72dd602d6293295c3790187415850557fc2b7
Carlos <JoseCarlosCarnicero@gmail.com>
2024-01-25 17:14:34 +0100

    Se anida un array y se crea una funcion que lo recorre

--- src/main.js
+++ src/main.js
@@ -9,15 +9,41 @@ const invoice = {
         lastName: "Lacaca",
         age: 20
     },
-    total: 500,
+    items: [
+        {
+            producto:"keyboard",
+            precio:320,
+            cantidad: 2,
+        },
+        {
+            producto: "mouse",
+            precio: 21,
+            cantidad: 5
+        },
+        {
+            producto: "papel",
+            precio: 15,
+            cantidad: 50
+        },
+    ],
+    total: function(){
+        let total = 0;
+
+        this.items.forEach(item => {
+            total = total + item.precio * item.cantidad;
+        })
+        return total;
+    },
     greeting: function(){
         return `Hola ${this.cliente.name}`
     }
 
 };
 //se anida el objeto cliente con {} a invoice y a continiacion se muestr como modificarlo
-invoice.cliente.name= "Yolamo"
+//invoice.cliente.name= "Yolamo"
 console.log(invoice);
 
-const saludar = invoice.greeting();
-console.log(saludar);
+//const saludar = invoice.greeting();
+console.log(invoice.greeting());
+
+console.log(`Total ${invoice.total()}`);
