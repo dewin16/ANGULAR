a3c09714e3cafc89a7a9efd2f3da8cd83a33a95f
Carlos <JoseCarlosCarnicero@gmail.com>
2024-01-25 14:19:47 +0100

    Creacion de proyecto y explicacion de variables

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+//let es una variable que puede cambiar
+let name = 'pedro';
+name = "andres";
+
+//const son valores inmutables
+const lastName = "Picapiedra";
+const condicion = true;
+
+if(condicion){
+    const lastName = "Chiquilicuatre";
+    console.log(lastName);
+}
+console.log("Hola mundo" + " " + name+ " " + lastName );
